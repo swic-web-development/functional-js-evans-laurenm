c1b6efc695f0a7618adc068052b2389f6fdcc57a
Lauren Evans <lauren.evans@student.swic.edu>
2025-03-06 20:42:08 -0600

    Add Callback Practice

--- src/index.js
+++ src/index.js
@@ -13,3 +13,28 @@ const greetings = () => 'Hello, World!'
 console.log(greetings())
 
 //Callback Functions Practice
+
+function doSomethingWithNumber(number, callbackFunction) {
+  return callbackFunction(number)
+}
+
+function divideByTwo(num) {
+  return num / 2
+}
+
+console.log(doSomethingWithNumber(6, divideByTwo))
+
+function double(num) {
+  return num * 2
+}
+
+doSomethingWithNumber(42, double)
+
+console.log(doSomethingWithNumber(42, double))
+
+function triple(num) {
+  return num * 3
+}
+doSomethingWithNumber(42, triple)
+
+console.log(doSomethingWithNumber(42, triple))
