d3b1d939c07375f8f8d951a709b797bab0aa4feb
Lauren Evans <lauren.evans@student.swic.edu>
2025-03-17 23:20:25 -0500

    Add Arrays Methods Practice

--- src/arrays-practice.js
+++ src/arrays-practice.js
@@ -14,3 +14,46 @@ const numberss = [1, 2, 3, 4, 5]
 const doubledd = numbers.map((number) => number * 2)
 
 console.log(doubledd)
+
+//Filter Method with implicit return
+
+const players = [
+  { name: 'DoomGuy', frags: 25, deaths: 0 },
+  { name: 'Slayer', frags: 25, deaths: 1 },
+  { name: 'Marine', frags: 12, deaths: 15 },
+  { name: 'Noob', frags: 0, deaths: 25 },
+]
+
+const playersWithMoreFragsThanDeaths = players.filter((player) => player.frags > player.deaths)
+console.log(playersWithMoreFragsThanDeaths)
+
+//Reduce Method
+
+const totalFrags = players.reduce((total, player) => {
+  return total + player.frags
+}, 0)
+console.log(totalFrags)
+
+//Find Method
+
+const firstPlayerWithMoreFragsThanDeaths = players.find((player) => player.frags > player.deaths)
+
+console.log(firstPlayerWithMoreFragsThanDeaths)
+
+//Some Method
+
+const hasFrags = players.some((player) => player.frags)
+
+console.log(hasFrags)
+
+//Every Method
+
+const allPlayersHaveMoreFragsThanDeaths = players.every((player) => player.frags > player.deaths)
+
+console.log(allPlayersHaveMoreFragsThanDeaths)
+
+//Sort Method
+
+const sortedPlayers = players.sort((a, b) => b.frags - a.frags)
+
+console.log(sortedPlayers)
